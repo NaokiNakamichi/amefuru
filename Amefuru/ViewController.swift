c82f1a7a3561a93ceb477c6b310670bfa8bcc163
Naoki Nakamichi <ronnao0516@gmail.com>
2020-08-16 17:35:10 +0900

    Initial Commit

---
+++ Amefuru/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amefuru
+//
+//  Created by 中道直基 on 2020/08/16.
+//  Copyright © 2020 中道直基. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
